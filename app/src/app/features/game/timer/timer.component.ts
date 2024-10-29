5ffecaa17dc3c2ae04908ffc4b50381b6230f6ac
Kateryna <katiaemme96@yahoo.it>
2024-10-29 10:53:06 +0100

    timer is functional now

--- app/src/app/features/game/timer/timer.component.ts
+++ app/src/app/features/game/timer/timer.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { interval, map, Observable, Subscription } from 'rxjs';
 import { GameService } from '../../../services/game.service';
 
 @Component({
@@ -11,12 +11,60 @@ export class TimerComponent implements OnInit {
   timer$!: Observable<number>;
   currentPlayer$!: Observable<string>;
 
+  totalSeconds: number = 60;
+  remainingSeconds: number = this.totalSeconds;
+  seconds: number = this.remainingSeconds;
+  private timerSubscription!: Subscription;
+
   constructor(private gameSvc: GameService) {}
 
-  ngOnInit() {
-    // this.timer$ = this.gameSvc.gameState$.pipe(map((state) => state.timer));
-    // this.currentPlayer$ = this.gameSvc.gameState$.pipe(
-    //   map((state) => state.currentPlayer)
-    // );
+  @ViewChild('progressCircle') progressCircle!: ElementRef<SVGCircleElement>;
+  circumference: number = 2 * Math.PI * 59;
+
+  ngOnInit(): void {
+    //   this.timer$ = this.gameSvc.gameState$.pipe(map((state) => state.timer));
+    //   this.currentPlayer$ = this.gameSvc.gameState$.pipe(
+    //     map((state) => state.currentPlayer)
+    //   );
+  }
+
+  ngAfterViewInit(): void {
+    this.initializeCircle();
+    this.startTimer();
+  }
+
+  ngOnDestroy(): void {
+    this.stopTimer();
+  }
+
+  private initializeCircle(): void {
+    const circle = this.progressCircle.nativeElement;
+    circle.style.strokeDasharray = `${this.circumference}`;
+    circle.style.strokeDashoffset = `${this.circumference}`;
+  }
+
+  private startTimer(): void {
+    this.timerSubscription = interval(1000).subscribe(() => {
+      this.remainingSeconds--;
+      this.updateTimer();
+
+      if (this.remainingSeconds <= 0) {
+        this.stopTimer();
+      }
+    });
+  }
+
+  private stopTimer(): void {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  private updateTimer(): void {
+    this.seconds = this.remainingSeconds;
+    const offset =
+      this.circumference -
+      (this.circumference * this.remainingSeconds) / this.totalSeconds;
+    this.progressCircle.nativeElement.style.strokeDashoffset = `${offset}`;
   }
 }
