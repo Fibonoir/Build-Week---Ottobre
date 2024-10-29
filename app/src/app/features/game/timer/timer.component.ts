e407c21476715d357a45b0960e11432e04138a32
Kateryna <katiaemme96@yahoo.it>
2024-10-29 12:44:54 +0100

    added

--- app/src/app/features/game/timer/timer.component.ts
+++ app/src/app/features/game/timer/timer.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { interval, map, Observable, Subscription } from 'rxjs';
 import { GameService } from '../../../services/game.service';
+import { TimerService } from '../../../services/timer.service';
 
 @Component({
   selector: 'app-timer',
@@ -14,9 +15,9 @@ export class TimerComponent implements OnInit {
   totalSeconds: number = 60;
   remainingSeconds: number = this.totalSeconds;
   seconds: number = this.remainingSeconds;
-  private timerSubscription!: Subscription;
+  timerSubscription!: Subscription;
 
-  constructor(private gameSvc: GameService) {}
+  constructor(gameSvc: GameService, timerSvc: TimerService) {}
 
   @ViewChild('progressCircle') progressCircle!: ElementRef<SVGCircleElement>;
   circumference: number = 2 * Math.PI * 59;
@@ -37,13 +38,13 @@ export class TimerComponent implements OnInit {
     this.stopTimer();
   }
 
-  private initializeCircle(): void {
+  initializeCircle(): void {
     const circle = this.progressCircle.nativeElement;
     circle.style.strokeDasharray = `${this.circumference}`;
     circle.style.strokeDashoffset = `${this.circumference}`;
   }
 
-  private startTimer(): void {
+  startTimer(): void {
     this.timerSubscription = interval(1000).subscribe(() => {
       this.remainingSeconds--;
       this.updateTimer();
@@ -54,17 +55,45 @@ export class TimerComponent implements OnInit {
     });
   }
 
-  private stopTimer(): void {
+  stopTimer(): void {
     if (this.timerSubscription) {
       this.timerSubscription.unsubscribe();
     }
   }
 
-  private updateTimer(): void {
+  updateTimer(): void {
     this.seconds = this.remainingSeconds;
     const offset =
       this.circumference -
       (this.circumference * this.remainingSeconds) / this.totalSeconds;
     this.progressCircle.nativeElement.style.strokeDashoffset = `${offset}`;
   }
+
+  // questo dovrebbe funzionare in concomitanza con il nostro timerService
+
+  // seconds: number = 0;
+  // timerSubscription!: Subscription;
+
+  // @ViewChild('progressCircle') progressCircle!: ElementRef<SVGCircleElement>;
+  // circumference: number = 2 * Math.PI * 59; // Circumference for the circle
+
+  // ngOnInit(): void {
+  //   this.timerSubscription = this.timerSvc.gameSvc.gameStateSubject.subscribe(state => {
+  //     this.seconds = state.timer;
+  //     this.updateCircle(state.timer);
+  //   });
+
+  //   this.timerSvc.startTimer(); // Avvia il timer all'inizio
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.timerSubscription.unsubscribe(); // Ferma la sottoscrizione quando il componente è distrutto
+  //   this.timerSvc.stopTimer(); // Ferma il timer quando il componente è distrutto
+  // }
+
+  // private updateCircle(remainingTime: number): void {
+  //   const offset = this.circumference - (this.circumference * remainingTime) / this.timerSvc.countdownInterval / 1000; // Assicurati di avere i secondi corretti
+  //   this.progressCircle.nativeElement.style.strokeDasharray = `${this.circumference}`;
+  //   this.progressCircle.nativeElement.style.strokeDashoffset = `${offset}`;
+  // }
 }
