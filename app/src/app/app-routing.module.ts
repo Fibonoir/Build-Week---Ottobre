e6f8d98e2a78f1ddcb20a05ea1d0b15b71871928
Kateryna <katiaemme96@yahoo.it>
2024-10-29 09:05:37 +0100

    timer inizialized

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'game', loadChildren: () => import('./features/game/game.module').then(m => m.GameModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/game/game.module').then((m) => m.GameModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
