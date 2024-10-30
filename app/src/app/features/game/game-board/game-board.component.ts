bfd24427c5ef3266bb89bf41e010cca0a09b774d
Andrea-LaVentura75 <laventuraandrea40@gmail.com>
2024-10-30 15:19:31 +0100

    animazione da completare

--- app/src/app/features/game/game-board/game-board.component.ts
+++ app/src/app/features/game/game-board/game-board.component.ts
@@ -16,6 +16,7 @@ export class GameBoardComponent implements OnInit, OnDestroy {
   lastMove: iMove | null = null;
   savedGames: iGame[] = [];
   savedGamesSubscription!: Subscription;
+  currentPlayer: string | null = null;
 
   constructor(
     public gameService: GameService,
@@ -24,13 +25,15 @@ export class GameBoardComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    // Crea una nuova partita all'inizio
     this.gameService.createGame();
 
     // Sottoscrizione allo stato del gioco
     this.gameSubscription = this.gameService.gameState$.subscribe((game) => {
       if (game) {
         this.game = game;
+
+        // Aggiorna il giocatore attivo
+        this.currentPlayer = game.currentPlayer;
 
         // Memorizza l'ultima mossa
         if (game.moves.length > 0) {
@@ -40,26 +43,20 @@ export class GameBoardComponent implements OnInit, OnDestroy {
         }
       }
 
-      // Controlla se c'è un vincitore e reindirizza ai risultati
       if (this.game?.winner) {
-        // Consente l'accesso alla pagina dei risultati
         this.authGuard.allowResultsAccess = true;
-
-        // Reindirizza alla pagina dei risultati
         setTimeout(() => {
           this.router.navigate(['/results']);
         }, 800);
       }
     });
 
-    // Carica i giochi salvati e sottoscrivi all'aggiornamento della lista
     this.gameService.loadSavedGames();
     this.savedGamesSubscription = this.gameService.savedGames$.subscribe(
       (games) => (this.savedGames = games)
     );
   }
 
-  // Metodo per verificare se una cella è l'ultima mossa
   isLastMove(cell: iCell): boolean {
     if (!this.lastMove) return false;
     if (cell.col !== this.lastMove.column) return false;
@@ -74,27 +71,22 @@ export class GameBoardComponent implements OnInit, OnDestroy {
     return false;
   }
 
-  // Gestore del click sulla colonna per eseguire una mossa
   onColumnClick(col: number): void {
     if (this.game && !this.game.isGameOver) {
-      console.log(col); // Debug: log della colonna cliccata
       this.gameService.makeMove(col);
     }
   }
 
-  // Metodo per resettare il gioco
   resetGame(): void {
     if (this.game) {
       this.gameService.resetGame();
     }
   }
 
-  // Annulla un gioco salvato
   cancelGame(id: string) {
     this.gameService.cancelGame(id);
   }
 
-  // Cleanup delle sottoscrizioni per evitare memory leaks
   ngOnDestroy(): void {
     if (this.gameSubscription) {
       this.gameSubscription.unsubscribe();
